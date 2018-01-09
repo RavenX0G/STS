4e3dc884f8ad236202df15f7f44dcf5a0a40e11e
Mogul <xiexiaomeng_001@163.com>
2018-01-09 21:23:39 +0800

    [STS]SplashActivity

--- app/src/main/java/com/mogul/xxm/sts/SplashActivity.java
+++ app/src/main/java/com/mogul/xxm/sts/SplashActivity.java
@@ -1,8 +1,15 @@
 package com.mogul.xxm.sts;
 
+import android.content.Intent;
+import android.os.Handler;
+
 import com.mogul.xxm.sts.base.BaseActivity;
+import com.mogul.xxm.sts.ui.MainActivity;
 
 public class SplashActivity extends BaseActivity {
+
+    private Handler mHandler;
+    private Runnable mRunnable;
 
     @Override
     protected boolean isShowBacking() {
@@ -26,7 +33,19 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                intoMainActivity();
+            }
+        };
+        mHandler.postDelayed(mRunnable, 2000);
+    }
 
+    private void intoMainActivity(){
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 
     @Override
---
+++ app/src/main/java/com/mogul/xxm/sts/ui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mogul.xxm.sts.ui;
+
+import com.mogul.xxm.sts.R;
+import com.mogul.xxm.sts.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int attachLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateViews() {
+
+    }
+}
